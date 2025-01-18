b68b5d0c8c86bfb6f1e8b3b57a79ddd2488d5c17
SaintAlmeida <103289195+SaintAlmeida@users.noreply.github.com>
2025-01-18 06:02:32 -0400

    Add files via upload

---
+++ Calculadora em Python/calculadora.py
@@ -0,0 +1,11 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.bgcolor = '#000'
+    page.window_resizable = False
+    page.window_width = 250
+    page.window_height = 380
+    page.title = 'Calculadora'
+    page.window_always_on_top = True
+
+ft.app(target = main)    
